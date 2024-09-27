086ceaf58ae16f78caf137ca074c372ebda76303
Enmanuel De los Santos Cruz <116897148+EnmaSantos@users.noreply.github.com>
2024-09-27 08:10:39 -0600

    Add folder structure and initial files

---
+++ folders.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Create subdirectories in the current directory (data_analysis)
+mkdir -p ./data ./notebooks ./scripts ./results/graphs
+
+echo "Empty folder structure created successfully inside 'data_analysis'."
